cc46705aba1b3d3e3f69d383ebe49fe1b25f3cc2
Magzhan <magzhankarataev02@gmail.com>
2024-05-23 12:57:45 +0500

    Tabs routing

---
+++ app/(tabs)/setting.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Setting: React.FC = () => {
+    return (
+        <View>
+            <Text>setting</Text>
+        </View>
+    )
+}
+
+export default Setting
---
+++ app/(tabs)/transaction.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const Transaction: React.FC = () => {
+    return (
+        <View>
+            <Text>transaction</Text>
+        </View>
+    );
+}
+
+export default Transaction;
