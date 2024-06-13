45cbb721a81cb4869bed78740e5114aac6a1ec2a
Magzhan <magzhankarataev02@gmail.com>
2024-06-13 11:46:51 +0500

    wallet creates

--- components/pages/wallet/Wallet.tsx
+++ components/pages/wallet/Wallet.tsx
@@ -1,33 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Pressable, Alert } from 'react-native';
+import { View, Text, Pressable, Alert, Button, FlatList } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import * as SecureStore from 'expo-secure-store';
-import axios from 'axios';
 import elliptic, { eddsa as EdDSA } from 'elliptic';
 
 const Wallet: React.FC = () => {
-    const [publicKey, setPublicKey] = useState('');
-    const [privateKey, setPrivateKey] = useState('');
-
+    const [wallets, setWallets] = useState<{ publicKey: string, privateKey: string }[]>([]);
     const ec = new EdDSA('ed25519');
 
     useEffect(() => {
-        // Check if keys already exist in SecureStore
-        const loadKeys = async () => {
-            const storedPrivateKey = await SecureStore.getItemAsync('privateKey');
-            const storedPublicKey = await SecureStore.getItemAsync('publicKey');
-            if (storedPrivateKey && storedPublicKey) {
-                setPrivateKey(storedPrivateKey);
-                setPublicKey(storedPublicKey);
-            } else {
-                generateKeyPair();
-            }
-        };
-
         loadKeys();
     }, []);
 
-    const generateKeyPair = async () => {
+    const loadKeys = async () => {
+        const keys = await SecureStore.getItemAsync('wallets');
+        if (keys) {
+            setWallets(JSON.parse(keys));
+        } else {
+            createWallet();
+        }
+    };
+
+    const createWallet = async () => {
         let secret: any;
 
         if (window.crypto && window.crypto.getRandomValues) {
@@ -45,12 +39,11 @@ const Wallet: React.FC = () => {
         const privateKeyHex = key.getSecret('hex');
         const publicKeyHex = key.getPublic('hex');
 
-        await SecureStore.setItemAsync('privateKey', privateKeyHex);
-        await SecureStore.setItemAsync('publicKey', publicKeyHex);
+        const newWallet = { publicKey: publicKeyHex, privateKey: privateKeyHex };
+        const updatedWallets = [...wallets, newWallet];
+        await SecureStore.setItemAsync('wallets', JSON.stringify(updatedWallets));
 
-        setPrivateKey(privateKeyHex);
-        setPublicKey(publicKeyHex);
-
+        setWallets(updatedWallets);
         requestTokens(publicKeyHex);
     };
 
@@ -68,39 +61,48 @@ const Wallet: React.FC = () => {
         }
     };
 
-    const handleCopyAddress = () => {
+    const handleCopyAddress = (publicKey: string) => {
         Alert.alert('Public Key', publicKey);
     };
 
-    return (
-        <>
-            <View style={{ display: 'flex', flexDirection: 'column', padding: 16, borderWidth: 1, borderColor: 'gray', borderRadius: 8 }}>
-                <View style={{ paddingBottom: 16, borderBottomWidth: 1, borderColor: 'gray', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                    <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', gap: 16 }}>
-                        <View style={{ width: 40, height: 40, borderRadius: 20, backgroundColor: 'violet' }}></View>
-                        <Text style={{ fontWeight: '500', fontSize: 18 }}>Account 1</Text>
-                    </View>
-                    <View>
-                        <Ionicons name="chevron-down-outline" size={24} color="black" />
-                    </View>
+    const renderWalletItem = ({ item }: { item: { publicKey: string, privateKey: string } }) => (
+        <View style={{ display: 'flex', flexDirection: 'column', padding: 16, borderWidth: 1, borderColor: 'gray', borderRadius: 8, marginBottom: 16 }}>
+            <View style={{ paddingBottom: 16, borderBottomWidth: 1, borderColor: 'gray', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', gap: 16 }}>
+                    <View style={{ width: 40, height: 40, borderRadius: 20, backgroundColor: 'violet' }}></View>
+                    <Text style={{ fontWeight: '500', fontSize: 18 }}>Account</Text>
                 </View>
-                <View style={{ paddingTop: 16, display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                    <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', gap: 16 }}>
-                        <Text style={{ fontWeight: '600' }}>Address:</Text>
-                        <Pressable
-                            style={{ borderRadius: 20, backgroundColor: '#6B96FE20', display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 8, padding: 8 }}
-                            onPress={handleCopyAddress}
-                        >
-                            <Text style={{ color: '#6B96FE' }}>{publicKey.slice(0, 6)}...{publicKey.slice(-4)}</Text>
-                            <Ionicons name="copy" size={16} color="#6B96FE" />
-                        </Pressable>
-                    </View>
-                    <View>
-                        <Ionicons name="ellipsis-horizontal" size={24} color="black" />
-                    </View>
+                <View>
+                    <Ionicons name="chevron-down-outline" size={24} color="black" />
                 </View>
             </View>
-        </>
+            <View style={{ paddingTop: 16, display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', gap: 16 }}>
+                    <Text style={{ fontWeight: '600' }}>Address:</Text>
+                    <Pressable
+                        style={{ borderRadius: 20, backgroundColor: '#6B96FE20', display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 8, padding: 8 }}
+                        onPress={() => handleCopyAddress(item.publicKey)}
+                    >
+                        <Text style={{ color: '#6B96FE' }}>{item.publicKey.slice(0, 6)}...{item.publicKey.slice(-4)}</Text>
+                        <Ionicons name="copy" size={16} color="#6B96FE" />
+                    </Pressable>
+                </View>
+                <View>
+                    <Ionicons name="ellipsis-horizontal" size={24} color="black" />
+                </View>
+            </View>
+        </View>
+    );
+
+    return (
+        <View style={{ padding: 16, height: '50%' }}>
+            <Button title="Create Wallet" onPress={createWallet} />
+            <FlatList
+                data={wallets}
+                renderItem={renderWalletItem}
+                keyExtractor={(item, index) => index.toString()}
+            />
+        </View>
     );
 };
 
