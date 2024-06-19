f6a7335cdee938d557c881c3183681ea957bb2fb
Magzhan <magzhankarataev02@gmail.com>
2024-06-19 18:28:37 +0500

    dfgdfg

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,15 @@
-import { Image, StyleSheet, Platform, View, Text, SafeAreaView } from 'react-native';
+import { Image, StyleSheet, Platform, View, Text, SafeAreaView, ScrollView } from 'react-native';
 import { Stack } from 'expo-router';
 import { WalletList, TabViews, Header } from '@/components/pages/wallet';
 
 export default function WalletScreen() {
   return (
-    <View style={{ flex: 1, padding: 16, gap: 16 }}>
-      <Header />
-      <WalletList />
-      <TabViews />
+    <View style={{ flex: 1 }}>
+      <ScrollView style={{ padding: 16, gap: 16 }}>
+        <Header />
+        <WalletList />
+        <TabViews />
+      </ScrollView>
     </View>
   );
 }
