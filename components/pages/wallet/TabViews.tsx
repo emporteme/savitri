c3ca66c0cea1b95d09c27186cbfe788ab717c3e5
MaksDie001 <anonrmv@gmail.com>
2024-07-04 16:30:59 +0500

    TaBviews_tabbar

--- components/pages/wallet/TabViews.tsx
+++ components/pages/wallet/TabViews.tsx
@@ -1,69 +1,87 @@
-import React from 'react'
-import { View, Text, useWindowDimensions, StyleSheet } from 'react-native'
+import React from 'react';
+import { View, Text, useWindowDimensions, StyleSheet } from 'react-native';
 import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 import Assets from './Assets';
 import Tokens from './Tokens';
 
 const FirstRoute = () => (
-  <Tokens />
+    <Tokens />
 );
 
 const SecondRoute = () => (
-  <Assets />
+    <Assets />
 );
 
 const renderScene = SceneMap({
-  first: FirstRoute,
-  second: SecondRoute,
+    first: FirstRoute,
+    second: SecondRoute,
 });
 
 const TabViews: React.FC = () => {
-  const layout = useWindowDimensions();
+    const layout = useWindowDimensions();
+    const screenWidth = layout.width;
+    const [index, setIndex] = React.useState(0);
+    const [routes] = React.useState([
+        { key: 'first', title: 'Tokens' },
+        { key: 'second', title: 'Assets & NFTs' },
+    ]);
 
-  const [index, setIndex] = React.useState(0);
-  const [routes] = React.useState([
-    { key: 'first', title: 'Tokens' },
-    { key: 'second', title: 'Assets & NFTs' },
-  ]);
-  return (
-    <>
-      <TabView
-        navigationState={{ index, routes }}
-        renderScene={renderScene}
-        onIndexChange={setIndex}
-        initialLayout={{ width: layout.width }}
-        renderTabBar={props => <TabBar
-          {...props}
-          renderLabel={({ focused, route }) => {
-            return (
-              <Text
-                style={{
-                  // fontSize: 16,
-                  color: focused ? '#6B96FE' : '#33333340',
-                  fontWeight: '600',
-                }}
-              >
-                {route.title}
-              </Text >
-            );
-          }}
-          indicatorStyle={styles.indicatorStyle}
-          style={styles.tabbar}
-        />}
-      />
-    </>
-  )
-}
+    return (
+        <>
+            <TabView
+                navigationState={{ index, routes }}
+                renderScene={renderScene}
+                onIndexChange={setIndex}
+                initialLayout={{ width: layout.width }}
+                renderTabBar={props => (
+                    <TabBar
+                        {...props}
+                        renderLabel={({ focused, route }) => (
+                            <View style={[focused ? styles.focusedTab : styles.defaultTab, { width: screenWidth / routes.length }]}>
+                                <View style={focused ? styles.focusedTab : styles.defaultTab}>
+                                    <Text
+                                        style={{
+                                            color: focused ? '#A84DFF' : '#C0C0C0',
+                                            fontWeight: '600',
+                                        }}
+                                    >
+                                        {route.title}
+                                    </Text>
+                                </View>
+                            </View>
+                        )}
+                        indicatorStyle={styles.indicatorStyle}
+                        style={styles.tabbar}
+                    />
+                )}
+            />
+        </>
+    );
+};
 
-export default TabViews
+export default TabViews;
 
 const styles = StyleSheet.create({
-  indicatorStyle: {
-    backgroundColor: '#6B96FE',
-    padding: 1.5,
-    marginBottom: -1.5,
-  },
-  tabbar: {
-    backgroundColor: 'transparent',
-  }
-})
+    focusedTab: {
+        backgroundColor: '#F1E4FF',
+        borderRadius: 8,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        alignItems:"center"
+    },
+    defaultTab: {
+        width:"100%",
+        backgroundColor: 'transparent',
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        alignItems:"center"
+    },
+    indicatorStyle: {
+        backgroundColor: 'transparent',
+    },
+    tabbar: {
+        backgroundColor: 'transparent',
+        elevation: 0,
+    },
+});
+
