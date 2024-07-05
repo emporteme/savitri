e9b261008eae5d69be06dff9a228cc170ebe512c
MaksDie001 <anonrmv@gmail.com>
2024-07-05 16:11:14 +0500

    token

--- components/pages/wallet/Tokens.tsx
+++ components/pages/wallet/Tokens.tsx
@@ -1,33 +1,49 @@
-import { View, Text, Pressable } from 'react-native'
-import React from 'react'
+import {View, Text, ScrollView, Image} from 'react-native';
+import React from 'react';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 const Tokens: React.FC = () => {
     return (
-        <View style={{ flex: 1, paddingVertical: 32, display: 'flex', flexDirection: 'column', gap: 32 }}>
-            <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-                <Text style={{ fontSize: 26, fontWeight: 400 }}>$0</Text>
+        <ScrollView contentContainerStyle={{ paddingVertical: 32, paddingHorizontal: 16 }}>
+            <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', marginBottom: 32 }}>
+                <Text style={{ fontSize: 26, fontWeight: 'bold' }}>$46,456.23</Text>
                 <View style={{ paddingHorizontal: 16, paddingVertical: 8, borderWidth: 1, borderColor: '#6B96FE', borderRadius: 20, display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 8 }}>
-                    <Text style={{ color: '#6B96FE', fontWeight: 500 }}>Portfolio</Text>
+                    <Text style={{ color: '#6B96FE', fontWeight: 'bold' }}>Portfolio</Text>
                     <Ionicons name="open-outline" size={16} color="#6B96FE" />
                 </View>
             </View>
-            <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-                <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', gap: 16 }}>
-                    <View style={{ width: 36, height: 36, borderRadius: 20, backgroundColor: 'violet' }}></View>
-                    <Text style={{ fontWeight: 400, fontSize: 18 }}>Savitri</Text>
-                </View>
-                <Text style={{ fontSize: 16, fontWeight: 600 }}>$0.00</Text>
+            <View style={{ display: 'flex', flexDirection: 'column', gap: 24 }}>
+                {[
+                    { name: 'Ikarus Token (IKT)', price: '$0.2', amount: 1000, value: '$200' },
+                    { name: 'Bitcoin (BTC)', price: '$0.2', amount: 1000, value: '$200' },
+                    { name: 'Tether (USDT)', price: '$0.2', amount: 1000, value: '$200' },
+                    { name: 'Token name (USDT)', price: '$0.2', amount: 1000, value: '$200' },
+                    { name: 'Token name (USDT)', price: '$0.2', amount: 1000, value: '$200' },
+                    { name: 'Token name (USDT)', price: '$0.2', amount: 1000, value: '$200' },
+                    { name: 'Token name (USDT)', price: '$0.2', amount: 1000, value: '$200' },
+                    { name: 'Ikarus Token (IKT)', price: '$0.2', amount: 1000, value: '$200' },
+                    { name: 'Bitcoin (BTC)', price: '$0.2', amount: 1000, value: '$200' },
+                    { name: 'Tether (USDT)', price: '$0.2', amount: 1000, value: '$200' },
+                    { name: 'Token name (USDT)', price: '$0.2', amount: 1000, value: '$200' },
+                    { name: 'Token name (USDT)', price: '$0.2', amount: 1000, value: '$200' },
+                    { name: 'Token name (USDT)', price: '$0.2', amount: 1000, value: '$200' },
+                    { name: 'Token name (USDT)', price: '$0.2', amount: 1000, value: '$200' },
+                ].map((token, index) => (
+                    <View key={index} style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                        <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 16 }}>
+                            <Image source={require('./img/savitri.png')} style={{ width: 36, height: 36, borderRadius: 18}}/>
+                            <Text style={{ fontWeight: 'bold', fontSize: 18 }}>{token.name}</Text>
+                        </View>
+                        <View style={{ display: 'flex', alignItems: 'flex-end' }}>
+                            <Text style={{ fontSize: 16, fontWeight: 'bold' }}>{token.amount}</Text>
+                            <Text style={{ fontSize: 14, color: '#888' }}>{token.value}</Text>
+                        </View>
+                    </View>
+                ))}
             </View>
-            <View style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-between', gap: 12 }}>
-                <Text style={{ fontWeight: 700, fontSize: 14 }}>Add Crypto to get started</Text>
-                <View style={{ backgroundColor: '#6B96FE', borderRadius: 24, padding: 12, flex: 1, width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                    <Text style={{ color: '#fff', fontSize: 14 }}>Buy Savitri</Text>
-                </View>
-                <Text style={{ textAlign: 'center', fontSize: 12 }}>Don't you see your token? <Text style={{ color: '#6B96FE' }}>Import tokens</Text></Text>
-            </View>
-        </View>
-    )
+        </ScrollView>
+    );
 }
 
-export default Tokens
+export default Tokens;
+
